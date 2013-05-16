aef852577017ca1ba4fb2c0818f6df412efa9a35
Ali Kargın <ali.sancopanco.com>
2013-05-16 19:09:12 +0300

    change Application controller to rescue access denied exception

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  #layout :select_layout
-  #rescue_from CanCan::AccessDenied do |exception|
-  #    redirect_to root_path, :alert => exception.message
-  #end
- 
-  private 
-  def select_layout
-     return 'application'  if user_signed_in? 
-     return  'beforelogin' 
+  rescue_from CanCan::AccessDenied do |exception|
+    flash[:error] = "Access denied."
+    redirect_to root_path, :alert => exception.message
   end
-  
 end
