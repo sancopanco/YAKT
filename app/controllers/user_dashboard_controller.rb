ccfae72f42bf46409eaf4c074a1155a7d15a6871
Ali Kargın <ali.sancopanco.com>
2013-05-16 19:12:41 +0300

    use rolify to get owning boards and member boards

--- app/controllers/user_dashboard_controller.rb
+++ app/controllers/user_dashboard_controller.rb
@@ -1,17 +1,11 @@
 class UserDashboardController < ApplicationController
   before_filter :authenticate_user!
   def index
-    #TODO:change this hard code
-    #binding.pry
-    @owning_boards = current_user.get_boards_by_role(Role::OWNER)
-    @member_boards = current_user.get_boards_by_role(Role::MEMBER)
+    @owning_boards = Board.with_role :owner, current_user
+    @member_boards = Board.with_role :member, current_user
     @new_board = Board.new
     render 'home/dashboard'
   end
   
-  def open_model_to_create_new_board
-    @new_board = Board.new
-    render :partial => 'home/new_board',:locals =>{:new_board => @new_board} 
-  end
   
 end
